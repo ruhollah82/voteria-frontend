af882ea0be7a9cbe92746388baa034a58d82275e
ruhollah82 <0robena0@gmail.com>
2026-05-30 23:47:03 +0330

    fix: wrap RouterProvider with TooltipProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import './index.css'
-import { router } from './app/router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import { router } from "./app/router";
+import { ThemeProvider } from "./app/providers/ThemeProvider";
+import { TooltipProvider } from "./components/ui/tooltip";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider>
+      <TooltipProvider>
+        <RouterProvider router={router} />
+      </TooltipProvider>
+    </ThemeProvider>
   </StrictMode>,
-)
+);
